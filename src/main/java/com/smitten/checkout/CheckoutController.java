e1dc08307b328a76bafb146cdb5cb49f95d6c5c4
smittenboutique <morganbmckaig@gmail.com>
2026-05-11 16:36:59 -0400

    fix missing spring imports

--- src/main/java/com/smitten/checkout/CheckoutController.java
+++ src/main/java/com/smitten/checkout/CheckoutController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 
