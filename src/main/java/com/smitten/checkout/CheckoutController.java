a17deb7a8386c2dd3820fe98b16c4b692c95e6ea
smittenboutique <morganbmckaig@gmail.com>
2026-05-11 18:02:56 -0400

    square api

--- src/main/java/com/smitten/checkout/CheckoutController.java
+++ src/main/java/com/smitten/checkout/CheckoutController.java
@@ -2,6 +2,14 @@ package com.smitten.checkout;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.List;
