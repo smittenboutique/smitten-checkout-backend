4dc57cd95f0af0577450e0c44bee9ebd045b4fdc
smittenboutique <morganbmckaig@gmail.com>
2026-05-12 20:21:28 -0400

    Update CheckoutController.java

--- src/main/java/com/smitten/checkout/CheckoutController.java
+++ src/main/java/com/smitten/checkout/CheckoutController.java
@@ -1,3 +1,8 @@
+package com.smitten.checkout;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class CheckoutController {
