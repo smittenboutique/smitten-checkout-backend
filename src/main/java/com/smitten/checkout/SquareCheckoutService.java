6d78187d98fd8ad54ff4986ccd5260795218e465
smittenboutique <morganbmckaig@gmail.com>
2026-05-13 23:01:25 -0400

    Update SquareCheckoutService.java

--- src/main/java/com/smitten/checkout/SquareCheckoutService.java
+++ src/main/java/com/smitten/checkout/SquareCheckoutService.java
@@ -90,7 +90,7 @@ public Map<String, Object> getCatalog() {
         HttpEntity<String> entity = new HttpEntity<>(headers);
         RestTemplate restTemplate = new RestTemplate();
 
-        String url = "squareBaseUrl() + "/v2/catalog/list?types=ITEM,ITEM_VARIATION,IMAGE";
+        String url = squareBaseUrl() + "/v2/catalog/list?types=ITEM,ITEM_VARIATION,IMAGE";
 
         ResponseEntity<Map> response =
                 restTemplate.exchange(url, HttpMethod.GET, entity, Map.class);
